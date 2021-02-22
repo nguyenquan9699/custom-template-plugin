e78fa887f7d5d715acfd1f643b618a5ce8bed481
GitHub Action <action@github.com>
2021-02-22 17:30:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/nguyenquan9699/customtemplateplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/nguyenquan9699/customtemplateplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nguyenquan9699.customtemplateplugin.listeners
 
+import com.github.nguyenquan9699.customtemplateplugin.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/nguyenquan9699/customtemplateplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nguyenquan9699.customtemplateplugin.services
+
+import com.github.nguyenquan9699.customtemplateplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
